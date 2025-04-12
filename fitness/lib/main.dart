9028f2d37de82a14224025793f20d484cb8b5f42
phattran2409 <phatt649@gmail.com>
2025-04-12 17:16:22 +0700

    initial

--- fitness/lib/main.dart
+++ fitness/lib/main.dart
@@ -4,3 +4,21 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const Quizstate());
 }
+
+class helloWidget extends StatelessWidget {
+  const helloWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello World'),
+        ),
+        body: const Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
